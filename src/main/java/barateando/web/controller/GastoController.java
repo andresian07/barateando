e34b510e6e54f564cf847deda0a0a9880677a15d
andresian07 <andresianzulu@gmail.com>
2026-09-18 15:23:58 +0200

    Unifica rutas de Gasto y Presupuesto a plural
    Mismo estilo que ya usan ViajeController y UsuarioController
    (/viajes, /usuarios) en vez de /gasto y /presupuesto en singular.

--- src/main/java/barateando/web/controller/GastoController.java
+++ src/main/java/barateando/web/controller/GastoController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/barateando/gasto")
+@RequestMapping("/barateando/gastos")
 public class GastoController {
     private final GastoService gastoService;
 
--- src/main/java/barateando/web/controller/PresupuestoController.java
+++ src/main/java/barateando/web/controller/PresupuestoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/barateando/presupuesto")
+@RequestMapping("/barateando/presupuestos")
 @RequiredArgsConstructor
 public class PresupuestoController {
     private final PresupuestoService presupuestoService;
